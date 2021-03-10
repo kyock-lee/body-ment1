b246a622bfc4ef7aa59821ebc939c935e953472f
kyock-lee <yakioni1031@gmailcom>
2021-03-11 01:00:34 +0900

    トップページのビューファイル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday])
+  end
 end
